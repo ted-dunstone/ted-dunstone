0670d897f381a74def5fb7813dafa0c64dbf0ae3
Ted Dunstone <ted@biometix.com>
2020-06-02 16:11:54 +0800

    Update start.js

--- EvieGame/src/start.js
+++ EvieGame/src/start.js
@@ -7,7 +7,7 @@ if(enablePWA) {
 	
 	if ('serviceWorker' in navigator) {
         	window.addEventListener('load', function() {
-            		navigator.serviceWorker.register('sw.js',{scope: '/'}).then(function(registration) {
+            		navigator.serviceWorker.register('sw.js',{scope: '/ted-dunstone/EvieGame/'}).then(function(registration) {
                 	console.log('ServiceWorker registration successful with scope: ', registration.scope);
 		    }, function(err) {
 			console.log('ServiceWorker registration failed: ', err);
