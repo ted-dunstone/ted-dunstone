76f558d97333f9b1b85818b32d20bbfe48612f48
Ted Dunstone <ted@biometix.com>
2020-06-02 15:58:34 +0800

    Update start.js

--- EvieGame/src/start.js
+++ EvieGame/src/start.js
@@ -7,7 +7,7 @@ if(enablePWA) {
 	
 	if ('serviceWorker' in navigator) {
         	window.addEventListener('load', function() {
-            		navigator.serviceWorker.register('./js/sw.js', {scope: '/'}).then(function(registration) {
+            		navigator.serviceWorker.register('src/sw.js', {scope: '/'}).then(function(registration) {
                 	console.log('ServiceWorker registration successful with scope: ', registration.scope);
 		    }, function(err) {
 			console.log('ServiceWorker registration failed: ', err);
