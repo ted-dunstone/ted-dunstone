f2280a864284669cdfabee71848e50734ddb3a2d
Ted Dunstone <ted@biometix.com>
2020-06-03 08:46:29 +0800

    update

--- EvieGame/src/start.js
+++ EvieGame/src/start.js
@@ -14,6 +14,31 @@ if(enablePWA) {
 		    });
 		});
 	 }
+
+	 let deferredPrompt;
+		window.addEventListener('beforeinstallprompt', function (e) {
+			console.log('beforeinstallprompt triggered');
+			e.preventDefault();
+			deferredPrompt = e;
+			modal.style.display = 'block';
+		});
+		// Get the modal
+		var modal = document.getElementById('myModal');
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName('close')[0];
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = 'none';
+			}
+		}
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+			modal.style.display = 'none';
+		}
+		function offlinePrompt() {
+			deferredPrompt.prompt();
+		}
 	// NOTIFICATIONS TEMPLATE
 	/*Promise.resolve(Notification.requestPermission()).then(function(permission) {
     		if(permission === 'granted') {
